1abf1e08c10ee89b4e2883d04d3aa95c7e43fc0b
hristo2612 <hristoapps@gmail.com>
2019-02-25 15:14:34 +0200

    Update demo.module + demo.component

--- demo/demo.module.ts
+++ demo/demo.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ConfirmationPopoverModule } from '../src';
 import { DemoComponent } from './demo.component';
+import { SharedModule } from '../src/shared/shared.module';
 
 @NgModule({
   declarations: [DemoComponent],
   imports: [
     BrowserModule,
     FormsModule,
+    SharedModule,
     ConfirmationPopoverModule.forRoot({
       focusButton: 'confirm'
     })
